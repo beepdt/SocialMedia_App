19c71705cbfa93310ba1da0578334622de9927e4
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2025-01-31 03:47:43 +0530

    server pt1

--- client/social-media-app/src/pages/LoginPage/Form.jsx
+++ client/social-media-app/src/pages/LoginPage/Form.jsx
@@ -52,7 +52,7 @@ const Form = ()=>{
     const isRegister = pageType === "register";
 
     const login = async (values, onSubmitProps) => {
-        const loggedInResponse = await fetch("http://localhost:5000/auth/login", {
+        const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(values),
@@ -79,7 +79,7 @@ const Form = ()=>{
         formData.append("picturePath", values.picture.name);
     
         const savedUserResponse = await fetch(
-          "http://localhost:5000/auth/register",
+          "http://localhost:3001/auth/register",
           {
             method: "POST",
             body: formData,
