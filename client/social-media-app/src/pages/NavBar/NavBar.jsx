987628319abea13657424d37d7fca8598e631557
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2025-02-01 02:57:24 +0530

    backend complete

--- client/social-media-app/src/pages/NavBar/NavBar.jsx
+++ client/social-media-app/src/pages/NavBar/NavBar.jsx
@@ -38,7 +38,7 @@ const NavBar = ()=>{
     const primaryLight = theme.palette.background.light;
     const alt = theme.palette.background.alt;
 
-    const fullName = "NerdyGlass";
+    const fullName = `${user.firstName} ${user.lastName}`;
     return(
       //background body
         <FlexBetween padding = "1rem 6%" backgroundColor = {alt} sx={{
