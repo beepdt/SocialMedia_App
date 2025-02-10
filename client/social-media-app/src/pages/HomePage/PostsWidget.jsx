84d77f21cfa9ffd5a734ef8ec044049177ac38c5
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2025-02-10 23:26:48 +0530

    all posts

---
+++ client/social-media-app/src/pages/HomePage/PostsWidget.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch,useSelector } from "react-redux";
+import { setPosts } from "../../state";
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({userId, isProfile = false}) => {
+    const dispatch = useDispatch();
+    const posts = useSelector((state)=> state.posts);
+    const token = useSelector((state)=> state.token);
+
+    const getPosts = async() => {
+        const response = await fetch
+    }
+}
+
+export default PostsWidget;
